d1fece616cbe53db922c41ed60068446751faf49
Chris <L25171185@culiacan.tecnm.mx>
2026-04-24 18:11:01 -0700

    First commit

---
+++ src/test/java/com/example/salespointproject/SalesPointProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salespointproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesPointProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
