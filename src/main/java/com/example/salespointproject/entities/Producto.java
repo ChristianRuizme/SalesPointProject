38ffc2bdd6402fcb22ca4593197469b461922813
Chris <Christianruizmendoza77@gmail.com>
2026-04-26 10:11:46 -0700

    Version POS 3

--- src/main/java/com/example/salespointproject/entities/Producto.java
+++ src/main/java/com/example/salespointproject/entities/Producto.java
@@ -1,16 +1,52 @@
 package com.example.salespointproject.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.*;   //Jakarta Persistence API ayuda con el ORM
+import jakarta.validation.constraints.*;    //Etiquetas de validacion
+import lombok.*;    //Setters, getters y constructores
 
 @Entity
 @Table(name = "productos")
+@Data  //Genera setters, getters, etc con Lombok
+@NoArgsConstructor // Genera el constructor vacío que necesita Spring
+@AllArgsConstructor //Genera el constructor lleno
 public class Producto {
     //Atributos
     @Id //Primary key para este atributo
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "producto_seq") //strategy: como genera valor
+    //generator: nombre interno en java, sequenceName: nombre en SQL, allocationSize: como incrementa
+    @SequenceGenerator(
+            name = "producto_seq",
+            sequenceName = "secuencia_productos",
+            allocationSize = 1 // Incrementa de 1 en 1
+    )
     private Long id;
-    private String descripcion;
-    private String nombre;
-    private Integer stock;
+
+    @NotBlank(message = "El código de barras es obligatorio") //En caso de error muestra message
+    @Column(nullable = false, unique = true)    //nullable: no puede estar vacio, unique: es unico
+    //name: cambia el nombre de columna, length: maximo caracteres, precision: num de digitos
+    //scale: num de decimales, updatable: es actualizable, columnDefinition: comando SQL
     private String codigoBarras;
-    private Double precio;
+
+    private String descripcion;
+
+    @NotBlank(message = "El nombre del producto no puede estar vacío")
+    @Column(nullable = false)
+    private String nombre;
+
+    @NotNull(message = "El stock es obligatorio")
+    @Min(value = 0, message = "El stock no puede ser menor a cero")
+    @Column(nullable = false)
+    private Integer stock;
+
+    @NotNull(message = "El precio de venta es obligatorio")
+    @Min(value = 0, message = "El precio de venta no puede ser negativo")
+    @Column(nullable = false)
+    private Double precioVenta;
+
+    @NotNull(message = "El precio de costo es obligatorio")
+    @Min(value = 0, message = "El precio de costo no puede ser negativo")
+    @Column(nullable = false)
+    private Double precioCosto;
+
+    private Boolean activo = true;
 }
