2465d48bb63c7331bd3baed2314d7f7ef80bf844
Chris <L25171185@culiacan.tecnm.mx>
2026-05-04 21:51:45 -0700

    Avance webtokens

---
+++ src/main/java/com/example/salespointproject/controllers/AuthController.java
@@ -0,0 +1,15 @@
+package com.example.salespointproject.controllers;
+
+//Imports de otros documentos
+import com.example.salespointproject.services.JwtService;
+
+//Imports de librerias
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+}
--- src/main/java/com/example/salespointproject/services/JwtService.java
+++ src/main/java/com/example/salespointproject/services/JwtService.java
@@ -4,10 +4,16 @@ package com.example.salespointproject.services;
 import com.example.salespointproject.model.entities.Usuario;
 
 //Imports de librerias
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 //Imports de java
+import java.security.Key;
+import java.util.Date;
 
 @Service
 public class JwtService {
@@ -19,7 +25,23 @@ public class JwtService {
     private long JWT_EXPIRATION;
 
     public String generateToken(Usuario usuario ) {
+        return Jwts.builder()
+                .setSubject(String.valueOf(usuario.getId()))
+                //
+                .claim("username", usuario.getName())
+                .claim("role", usuario.getRole())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
+    //Metodo para hacer mi llave a Key
+    private Key getSignInKey() {
+        //Convertimos nuestra llave a bytes
+        byte [] keyBytes = Decoders.BASE64.decode(JWT_KEY);
+        //Convertimos bytes a java.security.Key para el metodo signWith
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
  }
