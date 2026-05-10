7bf20c0e0ba1bb13ebf9ef0b34bc39e6cdbeb01c
Chris <Christianruizmendoza77@gmail.com>
2026-05-10 14:15:26 -0700

    Avance auth

--- sales-point-frontend/src/app/core/services/auth.service.ts
+++ sales-point-frontend/src/app/core/services/auth.service.ts
@@ -1,13 +1,21 @@
 //Imports de angular
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 //Imports de otros documentos
-
-import {Injectable} from '@angular/core';
+import { Usuario } from '../../shared/models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthService {
+
+  private apiUrl = 'http://localhost:8080/api/auth';
+
+  constructor(http: HttpClient) {   }
+
+  crearUsuario(usuario: Usuario): Observable<Usuario>
 
 }
